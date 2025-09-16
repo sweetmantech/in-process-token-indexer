e6b5ddf731792f1cd6ef354f689f8e97c9a59032
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-16 10:05:03 -0500

    remove unused network param.

--- indexer.mjs
+++ indexer.mjs
@@ -4,10 +4,10 @@ import { NETWORKS } from "./lib/rpc.js";
 
 async function indexAllNetworks() {
   // Start both the blockchain event indexer and the payments indexer
-  // const blockchainIndexers = Object.values(NETWORKS).map(indexEventsForNetwork);
-  const paymentsIndexer = indexPayments("base"); // Using base as the network name for payments
+  const blockchainIndexers = Object.values(NETWORKS).map(indexEventsForNetwork);
+  const paymentsIndexer = indexPayments();
 
-  await Promise.all([paymentsIndexer]);
+  await Promise.all([...blockchainIndexers, paymentsIndexer]);
 }
 
 indexAllNetworks().catch(console.error);
