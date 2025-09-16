cd1ffe108566da3ca4681a064cfc937037f333ac
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-16 09:42:11 -0500

    Cron API - Indexer => DB - in_process_payments

--- indexer.mjs
+++ indexer.mjs
@@ -1,8 +1,13 @@
 import indexEventsForNetwork from "./lib/indexEventsForNetwork.js";
+import indexPayments from "./lib/indexPayments.js";
 import { NETWORKS } from "./lib/rpc.js";
 
 async function indexAllNetworks() {
-  await Promise.all(Object.values(NETWORKS).map(indexEventsForNetwork));
+  // Start both the blockchain event indexer and the payments indexer
+  // const blockchainIndexers = Object.values(NETWORKS).map(indexEventsForNetwork);
+  const paymentsIndexer = indexPayments("base"); // Using base as the network name for payments
+
+  await Promise.all([paymentsIndexer]);
 }
 
 indexAllNetworks().catch(console.error);
