3bd10327610c7b223dc5d1b4dbb6e704f9088981
thomas jin <tech.eng.me@gmail.com>
2025-10-28 09:08:25 +1000

    Update indexer.mjs

--- indexer.mjs
+++ indexer.mjs
@@ -4,10 +4,9 @@ import indexMoments from "./lib/moment/indexMoments.js";
 async function indexAllNetworks() {
   // Start both the blockchain event indexer and the payments indexer
   const momentsIndexer = indexMoments();
-  // const paymentsIndexer = indexPayments();
+  const paymentsIndexer = indexPayments();
 
-  // await Promise.all([momentsIndexer, paymentsIndexer]);
-  await Promise.all([momentsIndexer, ]);
+  await Promise.all([momentsIndexer, paymentsIndexer]);
 }
 
 indexAllNetworks().catch(console.error);
