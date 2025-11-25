67eb6cbe5bb3b597008505f8d6e8ae9a9c5055e9
kcethdev <tech.eng.me@gmail.com>
2025-11-25 02:25:55 -0500

    remove dist

--- dist/legacy/lib/viem/getPublicClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const viem_1 = require("viem");
-const getChain_1 = require("./getChain");
-const getPublicClient = (chainId) => (0, viem_1.createPublicClient)({
-    chain: (0, getChain_1.getChain)(chainId),
-    transport: (0, viem_1.http)(),
-});
-exports.default = getPublicClient;
