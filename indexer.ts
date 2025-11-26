80a8f55a8cb048a2067ece21cdd2d7586ea8e379
kcethdev <tech.eng.me@gmail.com>
2025-11-26 00:39:56 -0500

    disable admins indexings

--- indexer.ts
+++ indexer.ts
@@ -3,8 +3,8 @@ import indexLegacyMoments from './legacy/lib/moment/indexMoments';
 import indexLegacyAdmins from './legacy/lib/moment/indexAdmins';
 import { executeMomentsIndexing } from './lib/moments/executeMomentsIndexing';
 import { executeCollectionsIndexing } from './lib/collections/executeCollectionsIndexing';
-import { executeMomentAdminsIndexing } from './lib/momentAdmins/executeMomentAdminsIndexing';
-import { executeCollectionAdminsIndexing } from './lib/collectionAdmins/executeCollectionAdminsIndexing';
+// import { executeMomentAdminsIndexing } from './lib/momentAdmins/executeMomentAdminsIndexing';
+// import { executeCollectionAdminsIndexing } from './lib/collectionAdmins/executeCollectionAdminsIndexing';
 import { executeMomentCommentsIndexing } from './lib/momentComments/executeMomentCommentsIndexing';
 import { executeSalesIndexing } from './lib/sales/executeSalesIndexing';
 
@@ -35,8 +35,8 @@ async function index(): Promise<void> {
   await Promise.all([
     executeCollectionsIndexing(),
     executeMomentsIndexing(),
-    executeMomentAdminsIndexing(),
-    executeCollectionAdminsIndexing(),
+    // executeMomentAdminsIndexing(),
+    // executeCollectionAdminsIndexing(),
     executeMomentCommentsIndexing(),
     executeSalesIndexing(),
   ]);
