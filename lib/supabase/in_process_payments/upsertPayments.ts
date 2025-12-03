162071d6f60978d406fb645b884df2d5144ce065
gm3 <tech.eng.me@gmail.com>
2025-12-04 09:00:16 +1000

    Update upsertPayments.ts

--- lib/supabase/in_process_payments/upsertPayments.ts
+++ lib/supabase/in_process_payments/upsertPayments.ts
@@ -20,7 +20,6 @@ export async function upsertPayments(
     .select();
 
   if (error) {
-    console.log('ziad here', payments);
     console.error('❌ Failed to upsert payments:', error);
     throw error;
   }
