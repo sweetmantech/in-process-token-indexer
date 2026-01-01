53133771b638a0e8eb89e8122cd841b983c11ef4
k.c <tech.eng.me@gmail.com>
2026-01-01 10:43:16 +1000

    track bug

--- lib/coinbase/getOrCreateSmartWallet.ts
+++ lib/coinbase/getOrCreateSmartWallet.ts
@@ -1,8 +1,14 @@
-import cdp from './client';
+// import cdp from './client';
 import { EvmSmartAccount } from '@coinbase/cdp-sdk';
 import { type Address } from 'viem';
 import { deterministicAccountName } from './deterministicAccountName';
 import { writeErrorToFile } from '../payments/writeErrorToFile';
+import { CdpClient } from '@coinbase/cdp-sdk';
+import {
+  CDP_API_KEY_ID,
+  CDP_API_KEY_SECRET,
+  CDP_WALLET_SECRET,
+} from '@/lib/consts';
 
 export async function getOrCreateSmartWallet({
   address,
@@ -10,9 +16,14 @@ export async function getOrCreateSmartWallet({
   address: Address;
 }) {
   try {
-    const evmAccount = await cdp.evm.getOrCreateAccount({
-      name: deterministicAccountName(address),
+    const cdp = new CdpClient({
+      apiKeyId: CDP_API_KEY_ID,
+      apiKeySecret: CDP_API_KEY_SECRET,
+      walletSecret: CDP_WALLET_SECRET,
     });
+    // const evmAccount = await cdp.evm.getOrCreateAccount({
+    //   name: deterministicAccountName(address),
+    // });
     // const smartAccount = await cdp.evm.getOrCreateSmartAccount({
     //   name: evmAccount.name as string,
     //   owner: evmAccount,
