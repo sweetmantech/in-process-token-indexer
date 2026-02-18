043288be6928ff560b81ad70fcfdc0fe30f9337e
k.c <tech.eng.me@gmail.com>
2026-02-18 11:39:47 +1000

    Revert "revert ecosystem.config.cjs fix"
    This reverts commit 4c534a225108c31330ca777aae34b5330425d19a.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'in-process-token-indexer',
-      script: 'npm',
-      args: 'run start',
+      script: 'node',
+      args: '--no-warnings --max-old-space-size=512 ./dist/indexer.js',
       exec_mode: 'fork',
-      node_args: '--max-old-space-size=512',
+      kill_timeout: 10000,
       max_memory_restart: '300M',
       cwd: '/root/in-process-token-indexer',
     },
