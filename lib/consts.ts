bdb6b44a2ed3254985777015685ea3aa128e3c9b
ziad <tech.eng.me@gmail.com>
2026-03-07 03:46:47 +1000

    update indexer id

--- lib/consts.ts
+++ lib/consts.ts
@@ -3,7 +3,7 @@ import { base, baseSepolia } from 'viem/chains';
 
 dotenv.config();
 
-export const INDEXER_ID = 'e6f7e28';
+export const INDEXER_ID = 'b41874b';
 export const GRPC_ENDPOINT = `https://indexer.dev.hyperindex.xyz/${INDEXER_ID}/v1/graphql`;
 
 export const SUPABASE_URL: string | undefined = process.env.SUPABASE_URL;
