f8ca0d185136d29774d7c8fda2c0bc04c20427b2
ziad <tech.eng.me@gmail.com>
2026-04-09 01:47:07 +1000

    revert

--- lib/indexers/indexers.ts
+++ lib/indexers/indexers.ts
@@ -16,22 +16,22 @@ import type { IndexConfig } from '@/types/factory';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const indexers: IndexConfig<any>[] = [
-  // // collections
-  // collectionsIndexer,
-  // catalogCollectionsIndexer,
+  // collections
+  collectionsIndexer,
+  catalogCollectionsIndexer,
   soundEditionsIndexer,
-  // // moments
+  // moments
   soundMomentsIndexer,
-  // momentsIndexer,
-  // catalogMomentsIndexer,
-  // // admins
-  // adminsIndexer,
-  // catalogAdminsIndexer,
-  // soundAdminsIndexer,
+  momentsIndexer,
+  catalogMomentsIndexer,
+  // admins
+  adminsIndexer,
+  catalogAdminsIndexer,
+  soundAdminsIndexer,
 
-  // commentsIndexer,
-  // salesIndexer,
-  // paymentsIndexer,
-  // airdropsIndexer,
-  // collectorsIndexer,
+  commentsIndexer,
+  salesIndexer,
+  paymentsIndexer,
+  airdropsIndexer,
+  collectorsIndexer,
 ];
